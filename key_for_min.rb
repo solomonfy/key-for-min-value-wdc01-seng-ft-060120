02d17e797de7d97098805d6015befe0207bbd90e
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-05-14 16:02:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,21 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 require "pry"
-# def key_for_min_value(name_hash)
-#   min_value = 100000000
-#   min_key = nil
-#   name_hash.each do |key, value|
-#     if value < min_value
-#       # binding.pry
-#       min_value = value
-#       min_key = key
-#       return key
-#     end
-#   end
-# min_key
-# end
-# # binding.pry
-
 
 def key_for_min_value(name_hash)
   min_value = 100000000
